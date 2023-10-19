60dbf0c3f05bc25f5d1624d73e626342988141ea
nihalmohd <nihalmohd828@gmail.com>
2023-10-19 21:46:15 +0530

    . enve is doen

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
+const dotenv = require("dotenv")
+app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.use(cors({
+    origin: process.env.BASEURL, 
+    methods: ["GET", "POST"],
+  }));
+  
+
+
+app.listen(PORT,()=>{
+    console.log("Server connected successfully on port " ,PORT);
+})
